07dc52b16291f1a0a487c801d432fd5b0081e959
LUMI <lmig@itu.dk>
2022-04-22 22:29:42 +0200

    tiding up

--- customerAPI.js
+++ customerAPI.js
@@ -1,19 +1,18 @@
 import express from "express";
 
 import {dbRouter} from "./customers/customer.route.js";
-import {quizzesRouter} from "./quizzes/quizzes.route.js";
+
 const app = express();
 const PORT = 3000;
 
 // This is a built-in middleware function in Express. It parses incoming requests with JSON payloads.
 app.use(express.json());
 
-// paths handled by quizzesRouter
+
 app.use(dbRouter)
 
-// new addition!
-// paths handled by quizzesRouter
-app.use(quizzesRouter)
+
+
 
 app.get("/", (req, res) => res.send("Server 3: Hello World!"));
 
